a1b76e01aa74938157e3115957524fc99c25e3b6
Matthew Batchelder <borkweb@gmail.com>
2015-03-19 13:42:28 -0400

    Update go-htmlbroom.php

--- go-htmlbroom.php
+++ go-htmlbroom.php
@@ -6,6 +6,8 @@
  * Version: 1.0
  * Author: Gigaom
  * Author URI: http://gigaom.com
+ * License: GPLv2
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  */
 
 require_once __DIR__ . '/components/class-go-htmlbroom.php';
